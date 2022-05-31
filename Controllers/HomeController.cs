08c2783e09dfae278281ade8da3340860d90dea8
iram96 <mariano.maselli@virgilio.it>
2022-05-31 15:27:35 +0200

    Aggiungere i file di progetto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(string Nome, string Desc, int Prezzo)
+
+        {
+
+            ViewData["Name"] = Nome;
+            ViewData["Desc"] = Desc;
+            ViewData["Price"] = Prezzo;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
